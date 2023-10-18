c38b853d543364dc9da445a6b2e58dd9f5ab2d59
saurdays <147225153+saurdays@users.noreply.github.com>
2023-10-18 17:54:06 +0700

    Add files via upload

---
+++ UTS_PEMVIS_B/src/uts_pemvis_b/UTS_PEMVIS_B.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts_pemvis_b;
+
+/**
+ *
+ * @author ASUS
+ */
+public class UTS_PEMVIS_B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
